dc325d3162e5456d8e69eb2058b42d85a91865c1
eleanor-rf <eleanorfrank@outlook.com>
2023-11-29 19:22:34 +0000

    complete day 6

---
+++ 006-karel.py
@@ -0,0 +1,21 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+"""
+Write a program using an if/elif/else statement so Reeborg can
+find the exit. The secret is to have Reeborg follow along the
+right edge of the maze, turning right if it can, going straight
+ahead if it can’t turn right, or turning left as a last resort.
+"""
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear() and not right_is_clear():
+        move()
+    else:
+        turn_left()
+        
